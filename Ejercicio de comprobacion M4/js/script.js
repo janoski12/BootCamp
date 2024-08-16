8ae205866ccddc9cc9116451ae4bcc2bc5b3ce51
janoski12 <alejandroacunab.12@gmail.com>
2024-08-16 19:46:31 -0400

    agegado de ultimas funciones

--- Ejercicio de comprobacion M4/js/script.js
+++ Ejercicio de comprobacion M4/js/script.js
@@ -43,3 +43,37 @@ class Carrito {
         return `Compra finalizada. El total es $${total}`;
     }
 }
+
+const productosDisponibles = [
+    new Producto('Pan', 1500),
+    new Producto('Leche', 1800),
+    new Producto('Huevos', 650)
+];
+
+const carrito = new Carrito();
+
+function iniciarCompra() {
+    let seguirComprando = true;
+
+    while (seguirComprando) {
+        const listaProductos = productosDisponibles.map((p, index) => `${index + 1}. ${p.nombre} - $${p.precio}`).join('\n');
+        const seleccion = parseInt(prompt(`Selecciona un producto:\n${listaProductos}`)) - 1;
+        const productoSeleccionado = productosDisponibles[seleccion];
+
+        if (productoSeleccionado) {
+            const cantidad = parseInt(prompt(`¿Cuantas unidades de ${productoSeleccionado.nombre} deseas comprar?`));
+            carrito.agregarProducto(productoSeleccionado, cantidad);
+            alert(`Has agregado ${cantidad} unidad(es) de ${productoSeleccionado.nombre} al carrito.`);
+
+            seguirComprando = confirm("¿Deseas agregar mas productos?");
+        } else {
+            alert("Selección invalida, intenta nuevamente.");
+        }
+    }
+
+    alert(carrito.mostrarDetalles());
+    alert(carrito.finalizarCompra());
+
+}
+
+iniciarCompra();
